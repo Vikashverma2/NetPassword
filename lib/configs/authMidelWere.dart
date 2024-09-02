c591e2677afb3d542357e2353fd753dbeddd2609
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2024-09-02 19:57:26 +0530

    added protected Route

---
+++ lib/configs/authMidelWere.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMiddleware extends GetMiddleware {
+  @override
+  RouteSettings? redirect(String? route) {
+    final auth = FirebaseAuth.instance;
+    final isLogin = auth.currentUser != null;
+
+    if (!isLogin && route != '/auth') {
+      return RouteSettings(name: '/auth');
+    }
+    return null;
+  }
+}
--- lib/configs/pageRoutes.dart
+++ lib/configs/pageRoutes.dart
@@ -1,31 +1,37 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:passwordmanager/pages/addNewPassword/addNewPassword.dart';
+import 'package:passwordmanager/configs/authMidelWere.dart';
 import 'package:passwordmanager/demo/demoPage.dart';
-import 'package:passwordmanager/pages/splacePage/splacePage.dart';
+import 'package:passwordmanager/pages/addNewPassword/addNewPassword.dart';
+import 'package:passwordmanager/pages/auth/authPage.dart';
+import 'package:passwordmanager/pages/homePage/homePage.dart';
 
-import '../pages/auth/authPage.dart';
-import '../pages/homePage/homePage.dart';
+import '../pages/splacePage/splacePage.dart';
 
 var pages = [
   GetPage(
     name: "/",
     page: () => SplacePage(),
+    middlewares: [AuthMiddleware()],
   ),
   GetPage(
     name: "/home",
     page: () => Homepage(),
+    middlewares: [AuthMiddleware()],
   ),
   GetPage(
+    
     name: "/auth",
     page: () => AuthPage(),
   ),
   GetPage(
+
     name: "/demo",
     page: () => DemoPage(),
+    middlewares: [AuthMiddleware()],
   ),
   GetPage(
     name: "/addnewpassword",
     page: () => AddNewPasswordPage(),
+    middlewares: [AuthMiddleware()],
   ),
 ];
