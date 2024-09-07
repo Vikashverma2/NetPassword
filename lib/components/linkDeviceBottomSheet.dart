b939705c9cc88e7c0f462a6bc646fb36b196c22d
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2024-09-07 15:33:19 +0530

    haptic feedback added

--- lib/components/linkDeviceBottomSheet.dart
+++ lib/components/linkDeviceBottomSheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -7,7 +8,7 @@ import 'package:passwordmanager/configs/colors.dart';
 
 Future<dynamic> LinkDeviceBottomSheet(BuildContext context) {
   return Get.bottomSheet(Container(
-    padding: EdgeInsets.all(20),
+    padding: const EdgeInsets.all(20),
     decoration:
         BoxDecoration(color: Theme.of(context).colorScheme.primaryContainer),
     child: Row(
@@ -17,7 +18,7 @@ Future<dynamic> LinkDeviceBottomSheet(BuildContext context) {
           child: Column(
             children: [
               Container(
-                padding: EdgeInsets.all(40),
+                padding: const EdgeInsets.all(40),
                 decoration: BoxDecoration(
                   color: lightBlueColor,
                   borderRadius: BorderRadius.circular(60),
@@ -28,7 +29,7 @@ Future<dynamic> LinkDeviceBottomSheet(BuildContext context) {
                   color: Theme.of(context).colorScheme.primary,
                 ),
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
@@ -38,32 +39,42 @@ Future<dynamic> LinkDeviceBottomSheet(BuildContext context) {
                   ),
                 ],
               ),
-              Text(
+              const Text(
                 "Don't share this code with anyone, this code can access your all save credential please keep this private. if you don't use code then please remove that",
                 style: TextStyle(
                   color: Colors.red,
                 ),
               ),
-              SizedBox(height: 20),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                decoration: BoxDecoration(
-                    border: Border.all(
+              const SizedBox(height: 20),
+              InkWell(
+                onLongPress: () {
+                  //  var password = decryptPassword(credential.hasPassword!);
+                  Clipboard.setData(const ClipboardData(text: "12356"));
+                  HapticFeedback.lightImpact();
+                },
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                      color: Theme.of(context)
+                          .colorScheme
+                          .primary
+                          .withOpacity(0.1)),
+                  child: Text(
+                    "76GS24DF",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
                       color: Theme.of(context).colorScheme.primary,
                     ),
-                    borderRadius: BorderRadius.circular(10),
-                    color:
-                        Theme.of(context).colorScheme.primary.withOpacity(0.1)),
-                child: Text(
-                  "76GS24DF",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.primary,
                   ),
                 ),
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Text(
                 "Write this code in other device",
                 style: Theme.of(context).textTheme.labelMedium,
